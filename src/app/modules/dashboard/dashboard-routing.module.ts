59018878f75e9dcb43793a4578175b140116a321
Karthik RP <blendedidea@gmail.com>
2020-05-03 20:29:32 +0530

    Added Invoice components.

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -5,6 +5,10 @@ import {DashboardBaseComponent} from './components/layout/dashboard-base/dashboa
 import {ListContactComponent} from './components/contact/list-contact/list-contact.component';
 import {CreateContactComponent} from './components/contact/create-contact/create-contact.component';
 import {ListProductsComponent} from './components/product/list-products/list-products.component';
+import {ListInvoiceComponent} from './components/invoice/list-invoice/list-invoice.component';
+import {CreateInvoiceComponent} from './components/invoice/create-invoice/create-invoice.component';
+import {EditInvoiceComponent} from './components/invoice/edit-invoice/edit-invoice.component';
+import {ViewInvoiceComponent} from './components/invoice/view-invoice/view-invoice.component';
 
 const CONTACT_ROUTES: Routes = [
   {path: 'list', component: ListContactComponent},
@@ -16,12 +20,20 @@ const PRODUCT_ROUTES: Routes = [
   {path: 'list', component: ListProductsComponent}
 ];
 
+const INVOICE_ROUTES: Routes = [
+  {path: 'list', component: ListInvoiceComponent},
+  {path: 'create', component: CreateInvoiceComponent},
+  {path: ':id/edit', component: EditInvoiceComponent},
+  {path: ':id/view', component: ViewInvoiceComponent},
+];
+
 const routes: Routes = [
   {
     path: '', component: DashboardBaseComponent, children: [
       {path: '', component: DashboardHomeComponent},
       {path: 'contact', children: CONTACT_ROUTES},
       {path: 'product', children: PRODUCT_ROUTES},
+      {path: 'invoice', children: INVOICE_ROUTES},
     ]
   },
 ];
