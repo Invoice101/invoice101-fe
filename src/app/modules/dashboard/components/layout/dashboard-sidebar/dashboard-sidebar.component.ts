af3e3cb4a5e6bff684c4f08af3f69b306c9694a3
Karthik RP <blendedidea@gmail.com>
2020-07-19 15:58:00 +0530

    Sidebar transitions.

--- src/app/modules/dashboard/components/layout/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/modules/dashboard/components/layout/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {faAddressBook, faDollarSign, faFilePdf, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import {faAddressBook, faDollarSign, faFilePdf, faShoppingBag} from '@fortawesome/free-solid-svg-icons';
+import {SessionService} from '../../../../../services/session.service';
+import {UserInterface} from '../../../../../interfaces/user.interface';
+import {AuthenticationService} from '../../../../../services/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-sidebar',
@@ -10,12 +14,22 @@ export class DashboardSidebarComponent implements OnInit {
   faAddressBook = faAddressBook;
   faFilePdf = faFilePdf;
   faDollarSign = faDollarSign;
-  faShoppingCart = faShoppingCart;
+  faShoppingBag = faShoppingBag;
 
-  constructor() {
+  user: UserInterface;
+
+  constructor(private sessionService: SessionService,
+              private authenticationService: AuthenticationService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
+    this.user = this.sessionService.user;
+  }
+
+  logout() {
+    this.authenticationService.clearCredentials();
+    this.router.navigate(['/login']);
   }
 
 }
